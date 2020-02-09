cd93874e061b26626d9272467d77afa1177b1b5d
Nikola Simovic <simovic.nikola90@gmail.com>
2020-02-09 09:36:49 +0100

    Updates

---
+++ precache-manifest.06614e4616d7a62faa3b0afab787b2cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0447a3267ce318f24471f04ea1e31ced",
+    "url": "/typing-game/index.html"
+  },
+  {
+    "revision": "7e51ba2c814ecdc9c960",
+    "url": "/typing-game/static/css/main.d6921285.chunk.css"
+  },
+  {
+    "revision": "23f62c6573c1a2c1f64b",
+    "url": "/typing-game/static/js/2.870bc302.chunk.js"
+  },
+  {
+    "revision": "b5a01430b9928300ee798657f45fe7d4",
+    "url": "/typing-game/static/js/2.870bc302.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e51ba2c814ecdc9c960",
+    "url": "/typing-game/static/js/main.4622638e.chunk.js"
+  },
+  {
+    "revision": "9acdf6bf13672e41ee49",
+    "url": "/typing-game/static/js/runtime-main.ee450915.js"
+  }
+]);
